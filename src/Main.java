d98cad7eac7a01460a2e3392523bb80576041746
OvidiuDorca <ovidiu.dorca@yahoo.com>
2019-05-23 20:45:46 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        // write your code here
+        char myChar = 'a';
+        byte myBte = 15;
+        int myInt = 129;
+
+
+    }
+}
